292fc08daeb7c941e5785ace050fe5ae9a3de7fa
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-24 17:27:12 +0200

    feat: metodos

--- src/net/salesianos/FigurasGeo/Circulo.java
+++ src/net/salesianos/FigurasGeo/Circulo.java
@@ -3,5 +3,13 @@ package net.salesianos.FigurasGeo;
 public class Circulo implements CalculoGeo {
 
     private double radio;
-    
+
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
 }
