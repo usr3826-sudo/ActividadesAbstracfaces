4f0e85f987e1becd062dad094000edb1c289d3d1
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-24 18:03:09 +0200

    Constructor de Vehiculo y metodos

--- src/net/salesianos/Vehiculos/Vehiculo.java
+++ src/net/salesianos/Vehiculos/Vehiculo.java
@@ -1,11 +1,22 @@
 package net.salesianos.Vehiculos;
 
 public abstract class Vehiculo {
-
+    
     protected String color;
     protected int puertas;
     protected int ruedas;
     protected String modelo;
     protected String matricula;
+
+    public Vehiculo(String color, int puertas, int ruedas, String modelo, String matricula) {
+        this.color = color;
+        this.puertas = puertas;
+        this.ruedas = ruedas;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
     
+    public abstract void arrancar();
+    public abstract void pararMotor();
+    public abstract void mover();
 }
