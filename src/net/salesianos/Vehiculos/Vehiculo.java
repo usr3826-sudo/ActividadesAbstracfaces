affd8507b9e142b1a56bcb92c25737e020a5b259
usr3826-sudo <usr3826@salesianos-lacuesta.net>
2026-04-24 17:54:56 +0200

    feat: Clase abstracta Vehhiculo y Atributos

---
+++ src/net/salesianos/Vehiculos/Vehiculo.java
@@ -0,0 +1,11 @@
+package net.salesianos.Vehiculos;
+
+public abstract class Vehiculo {
+
+    protected String color;
+    protected int puertas;
+    protected int ruedas;
+    protected String modelo;
+    protected String matricula;
+    
+}
